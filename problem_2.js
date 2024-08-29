9ad093a2ff3e8a369e5d20ddbefb6f2ee504d5bd
Ramkantodey <ramkantodey@gmail.com>
2024-08-29 14:55:08 +0600

    tast 2 solve

---
+++ problem_2.js
@@ -0,0 +1,9 @@
+var Mathematics, Biology,  Chemistry, Physics, Bangla, studentPoint;
+Mathematics = 75.25;
+Biology = 65;
+Chemistry = 80;
+Physics = 35.45;
+Bangla = 99.50;
+studentPoint = Mathematics + Biology + Chemistry + Physics + Bangla;
+studentPoint /= 5
+console.log("Avg No = ", studentPoint .toFixed(2));
